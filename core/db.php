16bd22706c36eb9399b58ce17ee812bbb0333205
Alexander Hill <me@alxhill.com>
2012-11-24 20:28:45 +0000

    Update core/db.php

--- core/db.php
+++ core/db.php
@@ -5,6 +5,6 @@ ActiveRecord\Config::initialize(function($cfg)
 {
 	$cfg->set_model_directory('application/models');
 	$cfg->set_connections(array(
-		'development' => 'mysql://root:##########m@localhost/test'
+		'development' => 'mysql://root:##########@localhost/test'
 	));
 });
